a3861c1ececcbda4d1ac3e2469c143e39a7eb35c
trumanfromkorea <jangjh971008@naver.com>
2022-06-18 03:49:57 +0900

    Initial Commit (repository 분리)

---
+++ StudyTimer/ViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudyTimer
+//
+//  Created by 장재훈 on 2022/06/07.
+//
+
+import FirebaseAuth
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
